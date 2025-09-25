1d0b5f8a27e1016b3ed4c372ddac0e271472152c
Aline <aline.rousselinsiv@gmail.com>
2025-09-25 15:34:25 +0900

    2025.09.25 first commit

---
+++ test1/src/main/java/com/example/test1/model/Student.java
@@ -0,0 +1,21 @@
+package com.example.test1.model;
+
+public class Student {
+	private String stuNo;
+	private String stuName;
+	private String stuDept;
+	private String stuGrade;
+	public String getStuNo() {
+		return stuNo;
+	}
+	public String getStuName() {
+		return stuName;
+	}
+	public String getStuDept() {
+		return stuDept;
+	}
+	public String getStuGrade() {
+		return stuGrade;
+	}
+	
+}
